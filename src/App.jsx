fd2e825856e66abb7a000fbeeaa3af5e5545ffc5
Aiko Zenser <aikozenser@gmail.com>
2026-08-03 09:57:26 +0200

    Druckansicht fehlerfrei abschließen

--- src/App.jsx
+++ src/App.jsx
@@ -15,5 +15,5 @@ export default function App(){
  const [favorites,setFavorites]=useLocalStorage('nyc-favorites',[])
  const select=i=>{setActive(i);setTimeout(()=>document.querySelector('.detail-layout')?.scrollIntoView({behavior:'smooth',block:'start'}),50)}
  return <><div className="app-shell"><Header onOpenChecklist={()=>setChecklist(true)}/><Hero/><DayCards days={trip} active={active} onSelect={select}/><DayDetail day={trip[active]} index={active} total={trip.length} onChange={select} favorites={favorites} setFavorites={setFavorites}/><Checklist open={checklist} onClose={()=>setChecklist(false)}/><PwaUpdate/>
- <footer><div><b>Unterkunft</b><br/>Residence Inn by Marriott<br/>1033 6th Ave</div><div><b>Notfall</b><br/>USA: 911</div><div><b>Reisezeit</b><br/>05.–12. August 2026</div></footer></div><PrintGuide days={trip}/>
+ <footer><div><b>Unterkunft</b><br/>Residence Inn by Marriott<br/>1033 6th Ave</div><div><b>Notfall</b><br/>USA: 911</div><div><b>Reisezeit</b><br/>05.–12. August 2026</div></footer></div><PrintGuide days={trip}/></>
 }
